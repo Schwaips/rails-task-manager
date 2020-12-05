f7a2e129ccedd383e3be48034fa382597de034f4
Charlie Bertrand <charlie.bertrand@live.com>
2020-12-05 13:56:00 +0100

    Task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,32 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+    # @checkbox = checkbok(@tasks)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params_task)
+    @task.save
+    redirect_to tasks_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def params_task
+    params.require(:task).permit(:title, :details)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+   # CREATE
+  get 'tasks/new' => 'tasks#new'
+  post 'tasks' => "tasks#create"
+
+  # VIEW
+  get 'tasks' => 'tasks#index'
+  get 'tasks/:id' => 'tasks#show'
+
+  # UPDATE
+  get 'tasks/:id/edit' => 'tasks#edit'
+  patch 'tasks'
+
 end
